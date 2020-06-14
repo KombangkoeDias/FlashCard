d8c65550024a968975588d5ef7a9f0b17f265a55
KBD <kbdirifilation@gmail.com>
2020-06-14 20:55:59 +0700

    scroll fixed, add controller to fields

--- lib/login.dart
+++ lib/login.dart
@@ -6,118 +6,134 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+
+  final UsernameController = TextEditingController();
+  final PasswordController = TextEditingController();
+
+  @override
+  void dispose(){
+    UsernameController.dispose();
+    PasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                width: 100,
-                height: 100,
-              ),
-              SizedBox(
-                width: 100.0,
-                height: 100.0,
-                child: Image(image: AssetImage('lib/Assets/logo.png')) //logo
-              ),
-              SizedBox(
-                width: 100,
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Log In",
-                    style: TextStyle(
-                      color: Colors.lightBlue,
-                      fontSize: 40
-                    )
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-                width: 100
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    width: 300,
-                    height: 60,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.lightBlue)
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey)
-                        ),
-                        hintText: 'Username'
-                      ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                ),
+                SizedBox(
+                  width: 100.0,
+                  height: 100.0,
+                  child: Image(image: AssetImage('lib/Assets/logo.png')) //logo
+                ),
+                SizedBox(
+                  width: 100,
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Log In",
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 40
+                      )
                     ),
-                  )
-                ],
-              ),
-              SizedBox(
-                width: 300,
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    width: 300,
-                    height: 60,
-                    child: TextField(
-                      decoration: InputDecoration(
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 300,
+                      height: 60,
+                      child: TextField(
+                        controller: UsernameController,
+                        decoration: InputDecoration(
                           focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.lightBlue)
+                            borderSide: BorderSide(color: Colors.lightBlue)
                           ),
                           enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.grey)
+                            borderSide: BorderSide(color: Colors.grey)
                           ),
-                          hintText: 'Password'
+                          hintText: 'Username'
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                  width: 300,
-                  height: 50,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                          'forget username or password?'
-                      ),
-                    ],
-                  )
-              ),
-              FlatButton(
-                color: Colors.lightBlue,
-
-                child: SizedBox(
-                  width: 100,
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Go!',
-                        style: TextStyle(
-                          fontSize: 20
-                        )
-                      ),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-                onPressed: () {},
-              )
-            ],
+                SizedBox(
+                  width: 300,
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 300,
+                      height: 60,
+                      child: TextField(
+                        obscureText: true,
+                        controller: PasswordController,
+                        decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.lightBlue)
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey)
+                            ),
+                            hintText: 'Password'
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                    width: 300,
+                    height: 50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                            'forget username or password?'
+                        ),
+                      ],
+                    )
+                ),
+                FlatButton(
+                  color: Colors.lightBlue,
+
+                  child: SizedBox(
+                    width: 100,
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Go',
+                          style: TextStyle(
+                            fontSize: 20
+                          )
+                        ),
+                      ],
+                    ),
+                  ),
+                  onPressed: () {print(UsernameController.text); print(PasswordController.text);},
+                )
+              ],
+            ),
           ),
         )
     );
