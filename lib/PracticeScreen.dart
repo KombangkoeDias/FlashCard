802544e5dc25c0c9df00636aa94340074cb80c0e
KBD <kbdirifilation@gmail.com>
2020-07-09 19:05:11 +0700

    Add practice screen and mechanism for adding progress and load progress

--- lib/PracticeScreen.dart
+++ lib/PracticeScreen.dart
@@ -14,6 +14,22 @@ class _PracticeScreenState extends State<PracticeScreen> {
   usernameSetname usernamesetname;
   FlashCardSets myset;
   flashCard currentCard;
+  String show = "";
+  String termOrDefinition = "";
+  int sequence = 1;
+
+  Future<List> addProgress(flashCard flashcard) async {
+    final response = await http.post(
+        "http://10.0.2.2/flash_card/addProgress.php",
+        body: {
+          "setName": usernamesetname.setName,
+          "username": usernamesetname.username,
+          "vocabulary": flashcard.word,
+          "meaning": flashcard.definition
+        }
+    );
+    print(response.body);
+  }
 
   Future<List> loadCurrentCard() async{
     final response = await http.post(
@@ -23,7 +39,20 @@ class _PracticeScreenState extends State<PracticeScreen> {
           "username": usernamesetname.username
         }
     );
-    print(response.body);
+    print("current card: " + response.body);
+    if (response.body.length == 2){
+      setState(() {
+        currentCard = myset.flashCardList[0];
+        print(currentCard.word);
+        show = currentCard.word;
+        print(currentCard.definition);
+        sequence = 1;
+      });
+    }
+    else{
+
+
+    }
   }
 
   Future<List> loadCards() async{
@@ -64,6 +93,22 @@ class _PracticeScreenState extends State<PracticeScreen> {
     for (int i = 0; i < myset.flashCardList.length; ++i){
       print("term: " +  myset.flashCardList[i].word + " ,meaning: " + myset.flashCardList[i].definition);
     }
+    loadCurrentCard();
+  }
+
+  void flip(){
+    if (show == currentCard.word){
+      setState(() {
+        show = currentCard.definition;
+        termOrDefinition = "term";
+      });
+    }
+    else{
+      setState(() {
+        show = currentCard.word;
+        termOrDefinition = "definition";
+      });
+    }
   }
 
   @override
@@ -76,13 +121,68 @@ class _PracticeScreenState extends State<PracticeScreen> {
             .arguments;
         myset = FlashCardSets(usernamesetname.username,[]);
         loadCards();
+        var a = "[word,definition]";
+        var newresponse = a.substring(1,a.length-1).split(',');
+        print(newresponse[0] + " " + newresponse[1]);
         value = false;
       });
     }
     return Scaffold(
       appBar: AppBar(
-        title: Text("Practice set : " + usernamesetname.setName)
+        title: Text("Practice set : " + usernamesetname.setName + " : " +
+            termOrDefinition + " " + sequence.toString())
       ),
+      body: Center(
+        child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                GestureDetector(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        child: Center(
+                          child: Text(
+                              show,
+                              style: TextStyle(
+                                  fontSize: 30
+                              )
+                          ),
+                        ),
+                        height: 300,
+
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                      flip();
+                  },
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: const Text('BACK'),
+                      color: Colors.blue,
+                      onPressed: () {print("Back");},
+                    ),
+                    RaisedButton(
+                      child: const Text('FLIP'),
+                      color: Colors.blue,
+                      onPressed: () {flip();},
+                    ),
+                    RaisedButton(
+                      child: const Text('NEXT'),
+                      color: Colors.blue,
+                      onPressed: () {print("Next");},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+      )
     );
   }
 }
